3830b5bd7e3dc28490be8e4ad575198d373f6972
minibr <minimin9823@gmail.com>
2024-09-22 19:10:31 +0900

    Preference Setting

---
+++ sofa/src/test/java/sofa/SofaApplicationTests.java
@@ -0,0 +1,13 @@
+package sofa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
