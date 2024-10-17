5f3ea3b380ca95e469d0cf0fad05b3552aa76db6
Jansoon <ckdals4600@naver.com>
2024-10-17 21:01:15 +0900

    Feat: 로그인 및 회원가입 기능 구현 (#1)

---
+++ src/main/java/com/sw19/sofa/domain/auth/dto/constants/AuthConstants.java
@@ -0,0 +1,5 @@
+package com.sw19.sofa.domain.auth.dto.constants;
+
+public class AuthConstants {
+    public static final String EMAIL_REGEXP = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,3}$";
+}
