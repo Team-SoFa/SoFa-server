85a949731e4ccdada4fb3ef4cdd9ed61814fe034
Jansoon <ckdals4600@naver.com>
2024-10-30 19:34:20 +0900

    Feat: Url로 Article 조회 매서드 추가 (#5)

---
+++ src/main/java/com/sw19/sofa/domain/article/service/ArticleService.java
@@ -0,0 +1,20 @@
+package com.sw19.sofa.domain.article.service;
+
+import com.sw19.sofa.domain.article.entity.Article;
+import com.sw19.sofa.domain.article.error.ArticleErrorCode;
+import com.sw19.sofa.domain.article.repository.ArticleRepository;
+import com.sw19.sofa.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Article getArticleByUrl(String url){
+        return articleRepository.findByUrl(url).orElseThrow(() -> new BusinessException(ArticleErrorCode.NOT_FOUND_ARTICLE));
+    }
+}
