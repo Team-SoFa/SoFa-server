88612ba332c949ce3cb0e66db0eaf1cda7b55590
Jansoon <ckdals4600@naver.com>
2024-10-30 19:37:26 +0900

    Feat: IdList로 MemberTagList 조회 매서드 추가 (#5)

--- src/main/java/com/sw19/sofa/domain/memberTag/service/MemberTagService.java
+++ src/main/java/com/sw19/sofa/domain/memberTag/service/MemberTagService.java
@@ -1,9 +1,11 @@
 package com.sw19.sofa.domain.memberTag.service;
 
+import com.sw19.sofa.domain.member.entity.MemberTag;
 import com.sw19.sofa.domain.memberTag.repository.MemberTagRepository;
 import com.sw19.sofa.global.common.dto.MemberTagDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,8 +14,14 @@ import java.util.List;
 public class MemberTagService {
     private final MemberTagRepository memberTagRepository;
 
+    @Transactional(readOnly = true)
     public List<MemberTagDto> getMemberTagDtoListByLinkCardId(Long linkCardId){
         return memberTagRepository.findAllByLinkCardId(linkCardId).stream()
                 .map(MemberTagDto::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberTag> getMemberTagListById(List<Long> idList){
+        return memberTagRepository.findAllById(idList);
     }
 }
