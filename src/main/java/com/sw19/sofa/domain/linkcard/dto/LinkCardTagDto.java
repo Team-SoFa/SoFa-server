4fa7c0fbc8ea48bb317dcd72abe9545ce50d4b11
Jansoon <ckdals4600@naver.com>
2024-11-07 02:22:47 +0900

    Feat: 태그 DTO 분리 (#5)

--- src/main/java/com/sw19/sofa/domain/linkcard/dto/LinkCardTagDto.java
+++ src/main/java/com/sw19/sofa/domain/linkcard/dto/LinkCardTagDto.java
@@ -1,20 +1,19 @@
 package com.sw19.sofa.domain.linkcard.dto;
 
-import com.sw19.sofa.domain.linkcard.dto.enums.TagType;
-import com.sw19.sofa.global.common.dto.MemberTagDto;
+import com.sw19.sofa.domain.linkcard.enums.TagType;
+import com.sw19.sofa.global.common.dto.CustomTagDto;
 import com.sw19.sofa.global.common.dto.TagDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LinkCardTagDto(
-        @Schema(name = "태그 아이디") String id,
-        @Schema(name = "태그명") String name,
-        @Schema(name = "태그 속성") TagType tagType
+        @Schema(description = "태그 아이디") String id,
+        @Schema(description = "태그명") String name,
+        @Schema(description = "태그 속성", example = "AI/CUSTOM") TagType tagType
 ) {
     public LinkCardTagDto(TagDto tagDto){
         this(tagDto.getEncryptId(), tagDto.name(), TagType.AI);
     }
-
-    public LinkCardTagDto(MemberTagDto memberTagDto){
-        this(memberTagDto.getEncryptId(), memberTagDto.name(), TagType.Member);
+    public LinkCardTagDto(CustomTagDto customTagDto){
+        this(customTagDto.getEncryptId(), customTagDto.name(), TagType.CUSTOM);
     }
 }
---
+++ src/main/java/com/sw19/sofa/domain/linkcard/dto/LinkCardTagSimpleDto.java
@@ -0,0 +1,14 @@
+package com.sw19.sofa.domain.linkcard.dto;
+
+import com.sw19.sofa.domain.linkcard.entity.LinkCardTag;
+import com.sw19.sofa.domain.linkcard.enums.TagType;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record LinkCardTagSimpleDto(
+        @Schema(description = "태그 아이디") Long id,
+        @Schema(description = "태그 속성", example = "AI/CUSTOM") TagType tagType
+) {
+    public LinkCardTagSimpleDto(LinkCardTag linkCardTag){
+        this(linkCardTag.getTagId(), linkCardTag.getTagType());
+    }
+}
