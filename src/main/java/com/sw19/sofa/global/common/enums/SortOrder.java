a3da4c0876f25ae94e8b2d05cdf11b717586ab94
Jansoon <ckdals4600@naver.com>
2024-11-13 01:55:09 +0900

    Feat: 확정성 고려 링크카드 목록 정렬 기준 인터페이스 생성 (#11)

---
+++ src/main/java/com/sw19/sofa/global/common/enums/SortOrder.java
@@ -0,0 +1,16 @@
+package com.sw19.sofa.global.common.enums;
+
+import com.querydsl.core.types.Order;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SortOrder {
+    @Schema(name = "오름차순")
+    ASCENDING(Order.ASC),
+    @Schema(name = "내림차순")
+    DESCENDING(Order.ASC);
+    private final Order order;
+}
