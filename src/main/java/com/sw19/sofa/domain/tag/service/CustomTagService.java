aa0e1933ce0f68bb82a3f5c8e03bd435b5bafe9b
minibr <minimin9823@gmail.com>
2024-11-17 18:50:05 +0900

    Feat: CustomTag 서비스 추가

--- src/main/java/com/sw19/sofa/domain/tag/service/CustomTagService.java
+++ src/main/java/com/sw19/sofa/domain/tag/service/CustomTagService.java
@@ -1,9 +1,12 @@
 package com.sw19.sofa.domain.tag.service;
 
+import com.sw19.sofa.domain.tag.entity.CustomTag;
 import com.sw19.sofa.domain.tag.repository.CustomTagRepository;
 import com.sw19.sofa.global.common.dto.CustomTagDto;
+import com.sw19.sofa.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,7 +15,27 @@ import java.util.List;
 public class CustomTagService {
     private final CustomTagRepository customTagRepository;
 
-    public List<CustomTagDto> getCustomTagDtoListByIdList(List<Long> tagIdList){
+    @Transactional(readOnly = true)
+    public List<CustomTag> getAllCustomTagsByMember(Member member) {
+        return customTagRepository.findByMember(member);
+    }
+
+    @Transactional
+    public CustomTag createCustomTag(Member member, String name) {
+        CustomTag customTag = CustomTag.builder()
+                .member(member)
+                .name(name)
+                .build();
+        return customTagRepository.save(customTag);
+    }
+
+    @Transactional
+    public void deleteCustomTag(Long id) {
+        customTagRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<CustomTagDto> getCustomTagDtoListByIdList(List<Long> tagIdList) {
         return customTagRepository.findAllByIdIn(tagIdList).stream()
                 .map(CustomTagDto::new)
                 .toList();
--- src/main/java/com/sw19/sofa/domain/tag/service/TagService.java
+++ src/main/java/com/sw19/sofa/domain/tag/service/TagService.java
@@ -5,19 +5,22 @@ import com.sw19.sofa.domain.tag.repository.TagRepository;
 import com.sw19.sofa.global.common.dto.TagDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class TagService {
+public class TagService { //Tag 엔티티 조회/태그 ID를 기반 DTO 목록을 반환하는 기능 함.
     private final TagRepository tagRepository;
 
-    public List<Tag> getTagList(List<String> tagNameList){
+    @Transactional(readOnly = true)
+    public List<Tag> getTagList(List<String> tagNameList) {
         return tagRepository.findAllByNameIn(tagNameList);
     }
 
-    public List<TagDto> getTagDtoListByIdList(List<Long> tagIdList){
+    @Transactional(readOnly = true)
+    public List<TagDto> getTagDtoListByIdList(List<Long> tagIdList) {
         return tagRepository.findAllByIdIn(tagIdList).stream()
                 .map(TagDto::new)
                 .toList();
