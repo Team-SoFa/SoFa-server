9c13300a63d06a2da446d2226003c00ad4c6a857
Minibr <minimin9823@gmail.com>
2024-11-24 07:11:13 +0900

    Refactor: Removed Unused lines (#10)

--- src/main/java/com/sw19/sofa/domain/searchbox/controller/SearchBoxController.java
+++ src/main/java/com/sw19/sofa/domain/searchbox/controller/SearchBoxController.java
@@ -6,9 +6,6 @@ import com.sw19.sofa.domain.searchbox.dto.response.SearchBoxRes;
 import com.sw19.sofa.domain.searchbox.service.SearchBoxService;
 import com.sw19.sofa.global.common.dto.ListRes;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
