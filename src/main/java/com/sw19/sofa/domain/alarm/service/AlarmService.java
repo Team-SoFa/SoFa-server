fd0a848340b90e3b199c1fc996d2857637ebb9bf
Jansoon <ckdals4600@naver.com>
2024-12-01 04:50:59 +0900

    Feat: 리마인드 알림 추가 로직 (#14)

--- src/main/java/com/sw19/sofa/domain/alarm/service/AlarmService.java
+++ src/main/java/com/sw19/sofa/domain/alarm/service/AlarmService.java
@@ -1,11 +1,27 @@
 package com.sw19.sofa.domain.alarm.service;
 
+import com.sw19.sofa.domain.alarm.entity.Alarm;
+import com.sw19.sofa.domain.alarm.enums.AlarmType;
 import com.sw19.sofa.domain.alarm.repository.AlarmRepository;
+import com.sw19.sofa.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AlarmService {
     private final AlarmRepository alarmRepository;
+
+    @Transactional
+    public void addRemindAlarm(Member member, String content){
+        Alarm alarm = Alarm.builder()
+                .member(member)
+                .content(content)
+                .type(AlarmType.REMIND)
+                .isRead(Boolean.FALSE)
+                .build();
+
+        alarmRepository.save(alarm);
+    }
 }
