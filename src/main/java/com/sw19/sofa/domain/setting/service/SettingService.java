5b15577528d482d3eaedb9e7ab60c86ee734c937
Minibr <minimin9823@gmail.com>
2024-12-01 18:05:42 +0900

    Feat: Setting Service 추가 (#17)

--- src/main/java/com/sw19/sofa/domain/setting/service/SettingService.java
+++ src/main/java/com/sw19/sofa/domain/setting/service/SettingService.java
@@ -2,18 +2,26 @@ package com.sw19.sofa.domain.setting.service;
 
 import com.sw19.sofa.domain.member.entity.Member;
 import com.sw19.sofa.domain.setting.entity.Setting;
+import com.sw19.sofa.domain.setting.enums.AlarmType;
+import com.sw19.sofa.domain.setting.error.SettingErrorCode;
 import com.sw19.sofa.domain.setting.repository.SettingRepository;
+import com.sw19.sofa.global.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class SettingService {
     private final SettingRepository settingRepository;
 
     @Transactional
-    public void setNewUser(Member member){
+    public void setNewUser(Member member) {
+        if (settingRepository.existsByMember(member)) {
+            throw new BusinessException(SettingErrorCode.SETTING_ALREADY_EXISTS);
+        }
+
         Setting setting = Setting.builder()
                 .member(member)
                 .is_recommend(true)
@@ -22,5 +30,39 @@ public class SettingService {
                 .build();
 
         settingRepository.save(setting);
+    }
+
+    public Setting getMemberSetting(Member member) {
+        return settingRepository.findByMember(member)
+                .orElseThrow(() -> new BusinessException(SettingErrorCode.SETTING_NOT_FOUND));
+    }
+
+    @Transactional
+    public void toggleAlarm(Member member, AlarmType alarmType) {
+        Setting setting = getMemberSetting(member);
+
+        switch (alarmType) {
+            case REMIND -> setting.toggleRemindAlarm();
+            case RECOMMEND -> setting.toggleRecommendAlarm();
+            case NOTICE -> setting.toggleNoticeAlarm();
+            default -> throw new BusinessException(SettingErrorCode.INVALID_ALARM_TYPE);
+        }
+
+        settingRepository.save(setting);
+    }
+
+    public boolean isRemindAlarmEnabled(Member member) {
+        Setting setting = getMemberSetting(member);
+        return setting.getIs_remind();
+    }
+
+    public boolean isRecommendAlarmEnabled(Member member) {
+        Setting setting = getMemberSetting(member);
+        return setting.getIs_recommend();
+    }
+
+    public boolean isNoticeAlarmEnabled(Member member) {
+        Setting setting = getMemberSetting(member);
+        return setting.getIs_notice();
     }
 }
