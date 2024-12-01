45b6d32bba29de14f5c0db532ea979a6b1d7d978
Minibr <minimin9823@gmail.com>
2024-12-01 22:52:21 +0900

    Fix: Deleted unused code (#10)

--- src/main/java/com/sw19/sofa/domain/searchbox/dto/request/SearchBoxReq.java
+++
@@ -1,9 +0,0 @@
-package com.sw19.sofa.domain.searchbox.dto.request;
-
-import java.util.List;
-
-public record SearchBoxReq(
-        String keyword,           // 검색어
-        String encryptedFolderId, // 선택된 폴더 ID (optional)
-        List<String> encryptedTagIds  // 선택된 태그 ID 리스트 (optional)
-) {}
