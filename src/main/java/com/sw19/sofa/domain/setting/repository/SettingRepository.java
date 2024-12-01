785af3550c6643041ef6a0bbffa3e3e59c7156f7
Minibr <minimin9823@gmail.com>
2024-12-01 18:06:05 +0900

    Feat: Setting Repository 추가 (#17)

--- src/main/java/com/sw19/sofa/domain/setting/repository/SettingRepository.java
+++ src/main/java/com/sw19/sofa/domain/setting/repository/SettingRepository.java
@@ -1,9 +1,14 @@
 package com.sw19.sofa.domain.setting.repository;
 
+import com.sw19.sofa.domain.member.entity.Member;
 import com.sw19.sofa.domain.setting.entity.Setting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SettingRepository extends JpaRepository<Setting, Long> {
+    Optional<Setting> findByMember(Member member);
+    boolean existsByMember(Member member);
 }
