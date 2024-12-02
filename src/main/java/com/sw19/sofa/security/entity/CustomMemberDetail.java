baa0a9b6316261a0d58249013bf00f69fd7141fa
Minibr <minimin9823@gmail.com>
2024-12-03 02:00:03 +0900

    Feat: CustomMemberDetail 수정(#25)

--- src/main/java/com/sw19/sofa/security/entity/CustomMemberDetail.java
+++ src/main/java/com/sw19/sofa/security/entity/CustomMemberDetail.java
@@ -28,21 +28,21 @@ public record CustomMemberDetail(Member member, Authority authority) implements 
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
-
+    
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
