1ff0f262bbd68d0247df1f2413dfacdcf78d8c0e
Jansoon <ckdals4600@naver.com>
2024-12-05 01:17:05 +0900

    Feat: EnableJpaAuditing 추가 (#36)

--- src/main/java/com/sw19/sofa/SofaApplication.java
+++ src/main/java/com/sw19/sofa/SofaApplication.java
@@ -4,9 +4,11 @@ import com.sw19.sofa.domain.auth.config.GoogleOAuth2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GoogleOAuth2Config.class)
+@EnableJpaAuditing
 public class SofaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SofaApplication.class, args);
