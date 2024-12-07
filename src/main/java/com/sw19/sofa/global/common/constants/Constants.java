1cde992288d3187b4e539046bf4c053ac5d56e17
Minibr <minimin9823@gmail.com>
2024-12-08 00:07:38 +0900

    Feat: AI폴더 categorizing 필드 추가 (#47)

--- src/main/java/com/sw19/sofa/global/common/constants/Constants.java
+++ src/main/java/com/sw19/sofa/global/common/constants/Constants.java
@@ -1,5 +1,11 @@
 package com.sw19.sofa.global.common.constants;
 
+import java.util.List;
+
 public class Constants {
     public static final String recycleBinName="휴지통";
+
+    public static final List<String> DEFAULT_FOLDER_CATEGORIES = List.of(
+    //폴더이름들 추가.
+            );
 }
