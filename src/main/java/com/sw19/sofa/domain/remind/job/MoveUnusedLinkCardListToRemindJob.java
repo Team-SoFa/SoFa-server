d26332e2d1abf7d9bb9b3f44569b5d110fdf29e2
Jansoon <ckdals4600@naver.com>
2024-12-08 03:17:41 +0900

    Feat: 미사용 링크카드 리마인드함 이동 작업 Batch 적용 (#14)

--- src/main/java/com/sw19/sofa/domain/remind/job/MoveUnusedLinkCardListToRemindJob.java
+++ src/main/java/com/sw19/sofa/domain/remind/job/MoveUnusedLinkCardListToRemindJob.java
@@ -1,21 +1,43 @@
 package com.sw19.sofa.domain.remind.job;
 
-import com.sw19.sofa.domain.remind.service.RemindManageService;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import static com.sw19.sofa.global.common.constants.Constants.moveRemind;
+
 @Component
+@Slf4j
 public class MoveUnusedLinkCardListToRemindJob implements Job {
-    private RemindManageService remindManageService;
+    private org.springframework.batch.core.Job remindJob;
+    private JobLauncher jobLauncher;
+
     @Autowired
-    public void setRemindManageService(RemindManageService remindManageService) {
-        this.remindManageService = remindManageService;
+    public void setRemindJob(@Qualifier(moveRemind + "Job") org.springframework.batch.core.Job remindJob) {
+        this.remindJob = remindJob;
+    }
+
+    @Autowired
+    public void setJobLauncher(JobLauncher jobLauncher) {
+        this.jobLauncher = jobLauncher;
     }
 
     @Override
     public void execute(JobExecutionContext context) {
-        remindManageService.moveUnusedLinkListToRemind();
+        try {
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("curTime", System.currentTimeMillis()) // 고유한 파라미터 추가
+                    .toJobParameters();
+            jobLauncher.run(remindJob, jobParameters);
+            log.info("Remind Job executed successfully.");
+        } catch (Exception e) {
+            log.error("Error executing Remind Job", e);
+        }
     }
 }
--- src/main/java/com/sw19/sofa/domain/remind/service/RemindManageService.java
+++ src/main/java/com/sw19/sofa/domain/remind/service/RemindManageService.java
@@ -1,23 +1,17 @@
 package com.sw19.sofa.domain.remind.service;
 
-import com.sw19.sofa.domain.linkcard.entity.LinkCard;
-import com.sw19.sofa.domain.linkcard.service.LinkCardService;
 import com.sw19.sofa.domain.member.entity.Member;
 import com.sw19.sofa.domain.remind.dto.response.RemindRes;
 import com.sw19.sofa.domain.remind.entity.Remind;
 import com.sw19.sofa.domain.remind.enums.RemindSortBy;
 import com.sw19.sofa.global.common.dto.ListRes;
 import com.sw19.sofa.global.common.dto.enums.SortOrder;
-import com.sw19.sofa.global.scheduler.manager.SchedulerManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.SchedulerException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -25,34 +19,6 @@ import java.util.stream.Collectors;
 @Transactional(readOnly = true)
 public class RemindManageService {
     private final RemindService remindService;
-    private final LinkCardService linkCardService;
-    private final SchedulerManager schedulerManager;
-
-    @Transactional
-    public void moveUnusedLinkListToRemind() {
-        List<LinkCard> unusedLinkCardList = linkCardService.getUnusedLinkCardList();
-
-        Map<Member, List<LinkCard>> linksGroupedByMember = unusedLinkCardList.stream()
-                .collect(Collectors.groupingBy(link -> link.getFolder().getMember()));
-
-        linksGroupedByMember.forEach(
-                (member, linkCardList) ->
-                {
-                    // 빈 리마인드 함에 신규 링크카드 추가 시 새로운 스케줄러 시작
-                    if(remindService.getRemindListByMember(member).isEmpty()){
-                        String encryptUserId = member.getEncryptUserId();
-                        try {
-                            schedulerManager.startMemberRemindNotifyScheduler(encryptUserId);
-                        } catch (SchedulerException e) {
-                            log.error("Error stopping the scheduler for memberId: {}",encryptUserId, e);
-                        }
-                    }
-
-                    remindService.addAllByLinkCardListAndMember(linkCardList, member);
-                }
-        );
-
-    }
 
     public ListRes<RemindRes> getRemindList(
             Member member,
