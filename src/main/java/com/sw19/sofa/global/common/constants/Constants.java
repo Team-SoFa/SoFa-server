1c3172421aa8668c4d7b063abb89f78dd617398c
Jansoon <ckdals4600@naver.com>
2024-12-08 03:16:58 +0900

    Feat: Batch taskExecutor 설정 (#14)

--- src/main/java/com/sw19/sofa/global/common/constants/Constants.java
+++ src/main/java/com/sw19/sofa/global/common/constants/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String deleteRecycleBin = "deleteExpiredLinkCardListInRecycleBinJob";
     public static final String remindNotify = "RemindNotifyGroup";
     public static final String deleteExpiredAlarm = "DeleteAlarmGroup";
+    public static final int batchConcurrencyLimit = 5;
 }
